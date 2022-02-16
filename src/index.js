55ee38bbfefde99d013c7bb3e340b5ed69463d4e
Junior Programmer <progphyton@gmail.com>
2022-02-16 22:34:51 +0300

    feat: the task brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, cfg) {
+	let mask = [];
+	let line = str;
+		if (str.length % 2) {
+			return false;
+		} else {
+			let indx = 0;
+			mask = cfg.map((elm) => elm.join(''));
+			recicle();
+			function recicle() {
+				for (let val of mask) {
+					for (let i = 0; i < str.length; i += 2) {
+						if (line.includes(val)) {
+							line = line.replace(val, '');
+						} else {
+							break;
+						};
+					};
+				};
+				indx = indx + 1;
+				if (indx <= (mask.length) && line.length > 0) {
+					recicle();
+				};
+			};
+			return line.length === 0;
+		};
+};
